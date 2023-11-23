80028909713c3e46b48d22081e346d796fd163b0
Sabrina del Valle <is.sabrinadelvalle@gmail.com>
2023-11-22 21:46:10 -0400

    Update v1

--- src/app/input.jsx
+++ src/app/input.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css"
+import { ThreeDots } from  'react-loader-spinner'
 
 export default function Input() {
 
@@ -10,6 +11,7 @@ export default function Input() {
     const [listOfCompanies, setListOfCompanies] = useState([]);
     const [currentAddress, setCurrentAddress] = useState(false);
     const [company, setCompany] = useState();
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         Aos.init({ duration: 2000, mirror: true });
@@ -18,7 +20,8 @@ export default function Input() {
 
     async function onSubmit(event) {
         event.preventDefault();
-        console.log(formData.email)
+        setLoading(true)
+        //console.log(formData.email)
         setCurrentAddress(false)
         setCompany();
 
@@ -29,11 +32,12 @@ export default function Input() {
         .then(json => {
                 setListOfCompanies(json.records)
                 setCurrentAddress(true)
+                setLoading(false)
         })
     }
 
     const selectCompany = (e) => {
-        setCompany(listOfCompanies.filter(elem => elem.fields.company_name === e.target.value)[0].fields)
+        setCompany(listOfCompanies.filter(elem => elem.fields.company_name === e.target.value)[0].fields);
     }
 
     const companiesList = listOfCompanies.map((company) => (
@@ -53,21 +57,32 @@ export default function Input() {
         <p className="p-14">---------------------</p>
         <br />
         <form data-aos="fade-right" onSubmit={onSubmit}>
-            <input type="email" name="email" placeholder="insert email " className="border border-neutral-600  rounded-lg p-5 w-fit outline-none focus:border-neutral-300 focus:border-2 text-neutral-900 text-lg text-center placeholder-gray-800 opacity-50 m-12" onChange={e => { const targetName =  e.target.value; setFormData({formData, email: targetName}) }} value={ formData.email }/>
+            <input type="email" name="email" placeholder="insert email " className="border border-neutral-600  rounded-lg p-5 w-80 outline-none focus:border-neutral-300 focus:border-2 text-neutral-900 text-lg text-center placeholder-gray-800 opacity-50 m-12" onChange={e => { const targetName =  e.target.value; setFormData({formData, email: targetName}) }} value={ formData.email }/>
             <button type='submit' className="hidden">Submit</button>
         </form> 
         {
             currentAddress ?
             <div><label htmlFor="company-list"></label>
-            <select name="company" id="company-list" className="border rounded-lg p-5 text-lg w-72 bg-slate-100/50 outline-none focus:border-neutral-300 focus:border text-neutral-900 text-center placeholder-neutral-300 opacity-50 m-12">
+            <select name="company" id="company-list" className="border rounded-lg p-5 text-lg w-80 bg-neutral-100/50 outline-none focus:border-neutral-300 focus:border text-neutral-500 text-center placeholder-neutral-300 opacity-50 m-12" data-aos="zoom-in">
             <option value="" className="">choose company</option>
                 {companiesList}
-            </select></div>:<p></p>
+            </select></div>:loading?<div>
+                        <ThreeDots 
+                            height="60" 
+                            width="60" 
+                            radius="9"
+                            color="grey" 
+                            ariaLabel="three-dots-loading"
+                            wrapperStyle={{}}
+                            wrapperClassName=""
+                            visible={true}
+                        /> 
+                    </div>:<p></p>
         }
         <div className="p-10">
         {
             company?
-            <ul className="flex flex-row bg-slate-600/10 rounded-xl p-8 justify-center items-start text-neutral-500 shadow-xl">
+            <ul data-aos="zoom-in" className="flex flex-row bg-slate-600/10 rounded-xl p-8 justify-center items-start text-neutral-500 shadow-xl">
                 <div>
                     <li className="p-2">-</li>
                     <li className="p-2">𖡡</li>
